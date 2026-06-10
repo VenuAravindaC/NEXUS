d3c8fe54cf7b004a123c461c97d2ec66643991ef
Venu Aravind <venuaravindc@gmail.com>
2026-06-10 09:26:48 +0530

    feat : initialize spring Boot backend with Maven , JPA , postgreSQL , DevTools , Lombok

---
+++ backend/nexus-backend/src/main/java/com/nexus/nexusbackend/NexusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nexus.nexusbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusBackendApplication.class, args);
+	}
+
+}
