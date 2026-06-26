bec543423066b9c6332fea5bfa88c5ff1a8fd40f
Venu Aravind <venuaravindc@gmail.com>
2026-06-26 19:14:37 +0530

    feat : add DashboardPage with empty state and dark theme

---
+++ frontend/nexus-frontend/src/pages/DashboardPage.jsx
@@ -0,0 +1,20 @@
+function DashBoardPage(){
+    return(
+
+        <div className= "min-h-screen pb-20">
+            <div className="p-4 pt-8">
+                <h1 className="text-2xl font-bold">My Reminders</h1>
+            </div>
+
+            <div className="flex flex-col item-center justify-center mt-32 px-8 text-center">
+                <p className="text-4xl mb-4">🔔</p>
+                <h2 className="text-xl font-semibold mb-2">Welcome to NEXUS</h2>
+                <p className="text-gray-400">Worried about about forgetting tasks , NEXUS got you </p>
+            </div>
+
+        </div>
+
+    )
+
+}
+export default DashBoardPage
